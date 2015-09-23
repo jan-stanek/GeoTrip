4fa88539eba7d07c7fc632a0cb9f3e51885c97cf
Jan Staněk <stanej16@cvut.cz>
2015-09-23 18:03:12 +0200

    init

---
+++ src/main/java/cz/cvut/fit/geotrip/GeoTrip.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.cvut.fit.geotrip;
+
+/**
+ *
+ * @author jan
+ */
+public class GeoTrip {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
