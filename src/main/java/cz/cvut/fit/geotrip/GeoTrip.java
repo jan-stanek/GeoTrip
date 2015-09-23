c7e7a812d4fa682fa3f2a44dfa473341e2e4a8a7
Jan Staněk <stanej16@cvut.cz>
2015-09-23 22:26:09 +0200

    GUI

--- src/main/java/cz/cvut/fit/geotrip/GeoTrip.java
+++ src/main/java/cz/cvut/fit/geotrip/GeoTrip.java
@@ -5,17 +5,56 @@
  */
 package cz.cvut.fit.geotrip;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import org.mapsforge.core.graphics.GraphicFactory;
+import org.mapsforge.core.model.BoundingBox;
+import org.mapsforge.core.model.MapPosition;
+import org.mapsforge.core.util.LatLongUtils;
+import org.mapsforge.map.awt.AwtGraphicFactory;
+import org.mapsforge.map.layer.Layers;
+import org.mapsforge.map.layer.cache.FileSystemTileCache;
+import org.mapsforge.map.layer.cache.InMemoryTileCache;
+import org.mapsforge.map.layer.cache.TileCache;
+import org.mapsforge.map.layer.cache.TwoLevelTileCache;
+import org.mapsforge.map.layer.renderer.TileRendererLayer;
+import org.mapsforge.map.model.MapViewPosition;
+import org.mapsforge.map.model.Model;
+import org.mapsforge.map.reader.MapFile;
+import org.mapsforge.map.reader.ReadBuffer;
+import org.mapsforge.map.rendertheme.InternalRenderTheme;
+import org.mapsforge.map.swing.controller.MapViewComponentListener;
+import org.mapsforge.map.swing.controller.MouseEventListener;
+import org.mapsforge.map.swing.view.MapView;
+
 /**
  *
  * @author jan
  */
 public class GeoTrip {
 
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) { }
+        
+        
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+        
+        
     }
+
+
     
 }
--- src/main/java/cz/cvut/fit/geotrip/MainFrame.java
+++ src/main/java/cz/cvut/fit/geotrip/MainFrame.java
@@ -5,19 +5,124 @@
  */
 package cz.cvut.fit.geotrip;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import org.mapsforge.core.graphics.GraphicFactory;
+import org.mapsforge.core.model.BoundingBox;
+import org.mapsforge.core.model.MapPosition;
+import org.mapsforge.core.util.LatLongUtils;
+import org.mapsforge.map.awt.AwtGraphicFactory;
+import org.mapsforge.map.layer.Layers;
+import org.mapsforge.map.layer.cache.FileSystemTileCache;
+import org.mapsforge.map.layer.cache.InMemoryTileCache;
+import org.mapsforge.map.layer.cache.TileCache;
+import org.mapsforge.map.layer.cache.TwoLevelTileCache;
+import org.mapsforge.map.layer.renderer.TileRendererLayer;
+import org.mapsforge.map.model.MapViewPosition;
+import org.mapsforge.map.model.Model;
+import org.mapsforge.map.reader.MapFile;
+import org.mapsforge.map.reader.ReadBuffer;
+import org.mapsforge.map.rendertheme.InternalRenderTheme;
+import org.mapsforge.map.swing.controller.MapViewComponentListener;
+import org.mapsforge.map.swing.controller.MouseEventListener;
+import org.mapsforge.map.swing.view.MapView;
+
 /**
  *
  * @author jan
  */
-public class MainFrame extends javax.swing.JPanel {
-
+public class MainFrame extends javax.swing.JFrame {
+   
     /**
-     * Creates new form MainFrame
+     * Creates new form MainFram
      */
+    
+    private static final GraphicFactory GRAPHIC_FACTORY = AwtGraphicFactory.INSTANCE;
+    private static MapView mapView;
+    
     public MainFrame() {
         initComponents();
+
+        panelMapa.setBorder(new CompoundBorder(new EmptyBorder(6, 2, 2, 2), panelMapa.getBorder()));
+        
+        ReadBuffer.setMaximumBufferSize(6500000);
+
+        File dir = new File("D:\\Stahování\\GeoTrip");
+        File[] mapFiles = dir.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.toLowerCase().endsWith(".map");
+            }
+        });
+        
+        mapView = createMapView();
+        mapView.setLocation(3, 7);
+        mapView.setSize(panelMapa.getWidth()-6, panelMapa.getHeight()-11);
+        panelMapa.add(mapView);
+        
+        final BoundingBox boundingBox = addLayers(mapView, Arrays.asList(mapFiles));
+        final Model model = mapView.getModel();
+        
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+                byte zoomLevel = LatLongUtils.zoomForBounds(model.mapViewDimension.getDimension(), boundingBox, model.displayModel.getTileSize());
+                model.mapViewPosition.setMapPosition(new MapPosition(boundingBox.getCenterPoint(), zoomLevel));
+            }
+        });
     }
 
+    private static MapView createMapView() {
+        MapView mapView = new MapView();
+        mapView.getMapScaleBar().setVisible(true);
+        
+        mapView.addComponentListener(new MapViewComponentListener(mapView, mapView.getModel().mapViewDimension));
+
+        MouseEventListener mouseEventListener = new MouseEventListener(mapView.getModel());
+        mapView.addMouseListener(mouseEventListener);
+        mapView.addMouseMotionListener(mouseEventListener);
+        mapView.addMouseWheelListener(mouseEventListener);
+
+        return mapView;
+    }
+    
+    private static BoundingBox addLayers(MapView mapView, List<File> mapFiles) {
+        Layers layers = mapView.getLayerManager().getLayers();
+
+        BoundingBox result = null;
+        for (int i = 0; i < mapFiles.size(); i++) {
+            File mapFile = mapFiles.get(i);
+            TileRendererLayer tileRendererLayer = createTileRendererLayer(createTileCache(i), mapView.getModel().mapViewPosition, true, true, mapFile);
+            BoundingBox boundingBox = tileRendererLayer.getMapDataStore().boundingBox();
+            result = result == null ? boundingBox : result.extend(boundingBox);
+            layers.add(tileRendererLayer);
+        }
+        
+        //addCaches(layers);
+        
+        return result;
+    }
+
+    private static TileCache createTileCache(int index) {
+        TileCache firstLevelTileCache = new InMemoryTileCache(128);
+        File cacheDirectory = new File(System.getProperty("java.io.tmpdir"), "mapsforge" + index);
+        TileCache secondLevelTileCache = new FileSystemTileCache(1024, cacheDirectory, GRAPHIC_FACTORY);
+        return new TwoLevelTileCache(firstLevelTileCache, secondLevelTileCache);
+    }
+
+    private static TileRendererLayer createTileRendererLayer(TileCache tileCache, MapViewPosition mapViewPosition, boolean isTransparent, boolean renderLabels, File mapFile) {
+        TileRendererLayer tileRendererLayer = new TileRendererLayer(tileCache, new MapFile(mapFile), mapViewPosition, isTransparent, renderLabels, GRAPHIC_FACTORY);
+        tileRendererLayer.setXmlRenderTheme(InternalRenderTheme.OSMARENDER);
+        return tileRendererLayer;
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -27,7 +132,6 @@ public class MainFrame extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        panelMapa = new javax.swing.JPanel();
         jPanel8 = new javax.swing.JPanel();
         jPanel3 = new javax.swing.JPanel();
         jPanel5 = new javax.swing.JPanel();
@@ -62,19 +166,16 @@ public class MainFrame extends javax.swing.JPanel {
         fieldDelka = new javax.swing.JTextField();
         labelTrasovani = new javax.swing.JLabel();
         comboTrasovani = new javax.swing.JComboBox();
+        labelKm = new javax.swing.JLabel();
+        panelMapa = new javax.swing.JPanel();
 
-        panelMapa.setBorder(javax.swing.BorderFactory.createTitledBorder("Mapa"));
-
-        javax.swing.GroupLayout panelMapaLayout = new javax.swing.GroupLayout(panelMapa);
-        panelMapa.setLayout(panelMapaLayout);
-        panelMapaLayout.setHorizontalGroup(
-            panelMapaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 609, Short.MAX_VALUE)
-        );
-        panelMapaLayout.setVerticalGroup(
-            panelMapaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(800, 560));
+        addComponentListener(new java.awt.event.ComponentAdapter() {
+            public void componentResized(java.awt.event.ComponentEvent evt) {
+                MainFrame.this.componentResized(evt);
+            }
+        });
 
         jPanel8.setBorder(javax.swing.BorderFactory.createTitledBorder("Nastavení"));
 
@@ -307,13 +408,13 @@ public class MainFrame extends javax.swing.JPanel {
 
         jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder("Výlet"));
 
-        labelDelka.setLabelFor(fieldDelka);
-        labelDelka.setText("Délka (km):");
+        labelDelka.setText("Délka:");
 
-        labelTrasovani.setLabelFor(comboTrasovani);
         labelTrasovani.setText("Trasa pro:");
 
         comboTrasovani.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "chůze", "kolo", "auto" }));
+
+        labelKm.setText("km");
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -325,15 +426,20 @@ public class MainFrame extends javax.swing.JPanel {
                     .addComponent(labelTrasovani))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(fieldDelka)
-                    .addComponent(comboTrasovani, 0, 221, Short.MAX_VALUE)))
+                    .addComponent(comboTrasovani, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(fieldDelka, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(labelKm)
+                        .addGap(0, 0, Short.MAX_VALUE))))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(fieldDelka, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(labelDelka))
+                    .addComponent(labelDelka)
+                    .addComponent(labelKm))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(comboTrasovani, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -346,10 +452,8 @@ public class MainFrame extends javax.swing.JPanel {
             jPanel8Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addGroup(jPanel8Layout.createSequentialGroup()
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
             .addComponent(buttonNaplanovat, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         jPanel8Layout.setVerticalGroup(
             jPanel8Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -361,11 +465,24 @@ public class MainFrame extends javax.swing.JPanel {
                 .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(buttonNaplanovat)
-                .addGap(0, 242, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
+        panelMapa.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
+
+        javax.swing.GroupLayout panelMapaLayout = new javax.swing.GroupLayout(panelMapa);
+        panelMapa.setLayout(panelMapaLayout);
+        panelMapaLayout.setHorizontalGroup(
+            panelMapaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 455, Short.MAX_VALUE)
+        );
+        panelMapaLayout.setVerticalGroup(
+            panelMapaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 534, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
@@ -373,19 +490,28 @@ public class MainFrame extends javax.swing.JPanel {
                 .addComponent(jPanel8, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(panelMapa, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGap(6, 6, 6))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(panelMapa, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jPanel8, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jPanel8, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(panelMapa, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
+
+        pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void componentResized(java.awt.event.ComponentEvent evt) {//GEN-FIRST:event_componentResized
+        mapView.setSize(panelMapa.getWidth()-6, panelMapa.getHeight()-11);
+    }//GEN-LAST:event_componentResized
+
+    /**
+     * @param args the command line arguments
+     */
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton buttonNaplanovat;
@@ -410,8 +536,9 @@ public class MainFrame extends javax.swing.JPanel {
     private javax.swing.JSlider jSlider1;
     private javax.swing.JSlider jSlider2;
     private javax.swing.JLabel labelDelka;
+    private javax.swing.JLabel labelKm;
     private javax.swing.JLabel labelTrasovani;
-    private javax.swing.JPanel panelMapa;
+    public javax.swing.JPanel panelMapa;
     private javax.swing.JRadioButton radioNenalezene;
     private javax.swing.JRadioButton radioObtiznostMala;
     private javax.swing.JRadioButton radioObtiznostNezalezi;
